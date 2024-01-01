97cf3319b934dcfe022d73053fc6043bc21ba6bd
gunler <per_gunler@hotmail.com>
2024-01-01 13:54:35 +0100

    Update settings.h
    förklaring

--- settings.h
+++ settings.h
@@ -5,12 +5,14 @@
 #define NUM_BRIGHTNESS_LEVELS 3
 
 char cases[NUM_CASES][6] = {
-  {'0', '0', '1', '0', '0', '3'},   // All off, first LED controlled by switch
-  {'0', '1', '0', '0', 'B', 'd'},   // First LED on, LED strip color A
-  {'0', 'x', '0', '0', '0', 'd'},   // Second LED on, LED strip color B
-  {'0', '0', 'f', '0', 'C', 'd'},   // Third LED on, LED strip color C
-  {'0', '0', '0', '0', '0', '0'},   // Fourth LED on, LED strip color A
-  {'0', '0', 'b', '0', 'A', '1'},   // All on with xenon effect, LED strip color B
+//led1  led2  led3  led4  strip  dim     // x=xenon f=fade b=blink A,B,C = färg enligt ledcolor.h (endast på strip) d, dim med switch 1,2,3 ljustyrka
+//  1     x     f     b     A     d      // EX. led1 on, led2 xenon, led3 fade, led4 blink, strip färg A, Led dimmas med 3switchknapp
+  {'0',  '0',  '1',  '0',  '0',  '3'},   // Läge 1  
+  {'0',  '1',  '0',  '0',  'B',  'd'},   // Läge 2
+  {'0',  'x',  '0',  '0',  '0',  'd'},   // Läge 3
+  {'0',  '0',  'f',  '0',  'C',  'd'},   // Läge 4
+  {'0',  '0',  '0',  '0',  '0',  '0'},   // Läge 5
+  {'0',  '0',  'b',  '0',  'A',  '1'},   // Läge 6
 };
 
 int brightnessLevels[NUM_BRIGHTNESS_LEVELS] = {20, 100, 255}; // Brightness levels
